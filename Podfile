eabfc125e28bc12b1bc34ea12fda7bca36b7026d
jayaram G <43853836+Jayaram143@users.noreply.github.com>
2018-12-22 11:37:53 +0530

    Initial commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+
+platform :ios, '9.0'
+
+target 'RMessageExample' do
+  
+  use_frameworks!
+
+  pod "RMessage"
+
+
+
+
+
+
+end
---
+++ RMessageExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RMessageExample
+//
+//  Created by Jayaram G on 22/12/18.
+//  Copyright © 2018 Jayaram G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+    }
+
+    
+
+}
+
